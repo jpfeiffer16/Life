1b6247cb881e5ed46bcad42531eae41ea1527102
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-09-14 22:17:00 -0600

    Adding a test to ty it out

--- game.js
+++ game.js
@@ -5,13 +5,35 @@ const Game = function (configObj) {
 
   //Generate the structure
 
-  var tiles = [][];
-  
+  let tiles = [];
 
-  let changeTile = (coordsObj) => {
+  for (var i = 0; i < width; i++) {
+    let subLevel = []
+    tiles.push(subLevel);
+    for (var j = 0; j < height; j++) {
+      subLevel.push({
+        state: false
+      });
+    }
+  }
+  let changeTile = (coordsObj, state) => {
     let {x, y} = coordsObj;
     if (!x || !y) return;
+    tiles[x][y].state = state;
+  }
+
+  let toggleTile = (coordsObj) => {
+    let {x, y} = coordsObj;
+    if (!x || !y) return;
+    let tile = tiles[x][y];
+    tile.state = !tile.state;
+  }
+
+  return {
+    changeTile,
+    toggleTile,
+    tiles
   }
 }
 
-module.exports = new Game();
+module.exports = Game;
---
+++ test.js
@@ -0,0 +1,14 @@
+var Game = require('./game');
+
+let game = Game({
+  width: 100,
+  height: 100
+});
+
+console.log('Done');
+game.toggleTile({
+  x: 99,
+  y: 99
+});  
+// game.changeTile();
+// console.log(JSON.stringify(game.tiles));
